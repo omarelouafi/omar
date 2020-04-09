d200752c2d2e142cf50d495098732d088086816c
omarelouafi <62441875+omarelouafi@users.noreply.github.com>
2020-04-09 22:47:30 +0100

    Add files via upload

---
+++ devoire3BD.sql
@@ -0,0 +1,60 @@
+
+*******************************************************************************************************************************************************************
+OMAR ELOUAFI
+
+*******************************************************************************************************************************************************************
+TP2
+
+*******************************************************************************************************************************************************************
+Procédures Stockées
+
+*******************************************************************************************************************************************************************
+1)***********************************************************************************************
+
+CREATE OR REPLACE PROCEDURE ajoutPilote
+AS
+BEGIN
+INSERT INTO pilote VALUES('3452','ANDRE',  'NICE'  ,4250,NULL,  '12-DEC-2000');
+dbms_output.put_line('pilote cree');
+EXCEPTION
+when dup_val_on_index then
+dbms_output.put_line('error duplicate');
+END;
+
+
+
+2)***********************************************************************************************
+
+CREATE OR REPLACE PROCEDURE supprimePilote(nbr varchar)
+AS
+BEGIN
+DELETE FROM pilote WHERE nopilot=nbr;
+IF SQL%NOTFOUND THEN
+dbms_output.put_line('Pilote n’existe pas');
+ELSE
+dbms_output.put_line('Pilote supprimé avec succès');
+END IF;
+END;
+
+
+3)***********************************************************************************************
+
+CREATE OR REPLACE PROCEDURE affichePilote_N(n INTEGER)
+IS 
+cursor c_pilote is select nom from pilote ORDER BY nom;
+         v_pilote pilote%rowtype;
+begin
+    if  if (n < c_pilote%ROWCOUNT) then
+    open c_pilote;
+    loop
+        fetch c_pilote into v_pilote;
+        exit when c_pilote%notfound;   
+            dbms_output.put_line('pilote : '||v_pilote.nom);
+    end loop;
+    else
+    dbms_output.put_line('le nombre entrer est plus grand que le nombre des lignes'||c_pilote%ROWCOUNT);
+end;
+
+
+
+
